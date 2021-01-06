a8c86ccd72143b8f937c93ccdacd16c0b865511a
tema181421 <tema181421@yandex.ru>
2021-01-06 18:53:30 +0300

    Migrate to github

--- src/videos/models.py
+++ src/videos/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from app.models import TimestampedModel
 
 
-class VideoClip(TimestampedModel):
+class VideoClip(models.Model):
     channel = models.ForeignKey(
         'channels.Channel', verbose_name=_('Parent channel'),
         on_delete=models.DO_NOTHING,
@@ -13,3 +13,5 @@ class VideoClip(TimestampedModel):
     title = models.CharField(_('Clip title'), max_length=128)
     description = models.TextField()
     views_count = models.PositiveIntegerField()
+
+    file = models.FileField()
